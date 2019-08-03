9b2e001ae73bc0ba3e3b11d02167dc4a35741b04
934905589 <934905589@qq.com>
2019-08-03 14:39:36 +0800

    第一次提交

---
+++ src/main/java/org/java/web/GitController.java
@@ -0,0 +1,18 @@
+package org.java.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: 老哥
+ * @Date: 2019/8/3 11:55
+ * @Description: 这是哥上课用的案例
+ */
+@RestController
+public class GitController {
+
+    @GetMapping
+    public String init(){
+        return "hello...";
+    }
+}
